3c09f801cb435f72988d1b92fc9b450b33359cca
taruc <goozk-wm17@student.tarc.edu.my>
2019-08-15 20:57:19 +0800

    commit

--- app/src/main/java/com/example/assignment/HomeFragment.java
+++ app/src/main/java/com/example/assignment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.assignment;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DigitalClock;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
-
+    DigitalClock clock;
+    TextView textviewDay, textviewWeekday;
+    Typeface tf;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -21,7 +30,24 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        clock = view.findViewById(R.id.digitalClock);
+        textviewDay = view.findViewById(R.id.textView_date);
+        textviewWeekday = view.findViewById(R.id.textView_weekday);
+
+        tf = Typeface.createFromAsset(getActivity().getAssets(),"Caballar.ttf");
+        clock.setTypeface(tf);
+
+        String date = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        String weekday = new SimpleDateFormat("EEEE", Locale.getDefault()).format(new Date());
+
+        textviewDay.setText(date);
+        textviewDay.setTypeface(tf);
+
+        textviewWeekday.setText(weekday);
+        textviewWeekday.setTypeface(tf);
+        return view;
     }
 
 }
